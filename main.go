0edc20e56712ab64313bb9f27cab17ff58b5627c
Jerry Caupain <dawglife@gmail.com>
2017-09-14 00:40:32 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world! Google Cloud Rules!!!...again!!")
+	io.WriteString(w, "Hello world! Google Cloud Rules!!!...again!!...and againnnnnn!!!!")
 }
 
 func main() {
