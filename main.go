68c7818c63350339d06f58ef43513da8a2d32059
Jerry Caupain <dawglife@gmail.com>
2017-09-14 12:31:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!.....It's demo time!! 10:47")
+	io.WriteString(w, "Hello World!.....It's demo time!! 12:30")
 }
 
 func main() {
